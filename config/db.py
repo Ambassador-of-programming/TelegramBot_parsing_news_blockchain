b1208c2673915c8812f67be40801cb9f6c6222d6
aktifon <110515441+aktifon@users.noreply.github.com>
2022-11-19 23:24:07 +0600

    Update db.py

--- config/db.py
+++ config/db.py
@@ -1,7 +1,7 @@
-host = "ec2-52-211-232-23.eu-west-1.compute.amazonaws.com"
-db_name = "dfcqfbc30mklt1"
-user = "xocwczxhhpafmn"
-password = "e8e3d8e915ec1b901520aacfb51605dc682f47d5f8c525c22008061c49b3785b"
+host = "compute.amazonaws.com"
+db_name = "name"
+user = "user"
+password = "password"
 port = 5432
 
 
